51ed9c802f5d649ca1fe2ccd0e70c5e3f8bb10f5
Calvin Collins <calvinc@surveymonkey.com>
2015-08-22 16:23:29 -0400

    added images, room and dresser show up and monster can hide in dresser

--- static/js/game.js
+++ static/js/game.js
@@ -7,15 +7,21 @@ $( document ).ready(function() {
 	var stage = new PIXI.Container();
 
 	// create textures
-	var bunnyText = PIXI.Texture.fromImage('static/img/bunny.png');
+	var monsterText = PIXI.Texture.fromImage('static/img/monster.png');
 	var bookText = PIXI.Texture.fromImage('static/img/book.png');
+	var roomText = PIXI.Texture.fromImage('static/img/room.png');
+	var dresserText = PIXI.Texture.fromImage('static/img/dresser.png');
 
 	// create sprites from textures
-	var monster = new PIXI.Sprite(bunnyText);
+	var monster = new PIXI.Sprite(monsterText);
 	var book = new PIXI.Sprite(bookText);
+	var room = new PIXI.Sprite(roomText);
+	var dresser = new PIXI.Sprite(dresserText);
 
 	setupMonster();
 	setupBook();
+	setupRoom();
+	setupDresser();
 
 	//create keybindings
 	var left = keyboard(37),
@@ -26,15 +32,19 @@ $( document ).ready(function() {
 
 	//create checks
 	var isHiding = false;
+	var hidePressed = false;
 
 	//group hiding places
 	var hidingSpots = [
-		book
+		book,
+		dresser	
 	];
 
 	//add sprites to stage
-	stage.addChild(monster);
+	stage.addChild(room);
 	stage.addChild(book);
+	stage.addChild(dresser);
+	stage.addChild(monster);
 
 	// start animating
 	gameLoop();
@@ -43,7 +53,7 @@ $( document ).ready(function() {
 		requestAnimationFrame(gameLoop);
 
 		moveMonster();
-		checkCollisions();
+		hideMonster();
 
 		// render the container
 		renderer.render(stage);
@@ -60,21 +70,26 @@ $( document ).ready(function() {
 		if (monster.x > 800){
 			monster.x = 800;
 		}
-		if (monster.y < 300){
-			monster.y = 300;
+		if (monster.y < 280){
+			monster.y = 280;
 		}
 		if (monster.y > 400){
 			monster.y = 400;
 		}
 	}
 
-	function checkCollisions() {
+	function hideMonster() {
 		for (spot in hidingSpots){
 			var spot = hidingSpots[spot];
-			if (isFuzzyCollision(monster, spot)){
-				if (!isHiding){
-					monster.x = spot.x;
-					monster.y = spot.y;
+			if (isCollision(monster, spot)){
+				if (!isHiding && hidePressed){
+					//monster.x = spot.x;
+					//monster.y = spot.y;
+					stage.swapChildren(monster, spot);
+					isHiding = true;
+				} else if (isHiding && !hidePressed){
+					stage.swapChildren(monster, spot);
+					isHiding = false;
 				}
 			}
 		}
@@ -119,8 +134,10 @@ $( document ).ready(function() {
 	}
 	
 	left.press = function() {
-		monster.vx = -5;
-		monster.vy = 0;
+		if (!isHiding){
+			monster.vx = -5;
+			monster.vy = 0;
+		}
 	};
 
 	left.release = function() {
@@ -130,8 +147,10 @@ $( document ).ready(function() {
 	};
 
 	right.press = function() {
-		monster.vx = 5;
-		monster.vy = 0;
+		if (!isHiding){
+			monster.vx = 5;
+			monster.vy = 0;
+		}
 	};
 
 	right.release = function() {
@@ -141,8 +160,10 @@ $( document ).ready(function() {
 	};
 
 	up.press = function() {
-		monster.vx = 0;
-		monster.vy = -3;
+		if (!isHiding){
+			monster.vx = 0;
+			monster.vy = -3;
+		}
 	};
 
 	up.release = function() {
@@ -152,8 +173,10 @@ $( document ).ready(function() {
 	};
 
 	down.press = function() {
-		monster.vx = 0;
-		monster.vy = 3;
+		if (!isHiding){
+			monster.vx = 0;
+			monster.vy = 3;
+		}
 	};
 
 	down.release = function() {
@@ -162,27 +185,51 @@ $( document ).ready(function() {
 		}
 	};
 
-	function setupMonster(){ // center the sprite's anchor point
+	hide.press = function() {
+		hidePressed = true;
+	};
+
+	hide.release = function() {
+		hidePressed = false;
+	};
+
+	function setupMonster(){
 		monster.anchor.x = 0.5;
 		monster.anchor.y = 0.5;
-
-		// move the sprite to the center of the screen
 		monster.position.x = 100;
 		monster.position.y = 300;
 		monster.vx = 0;
 		monster.vy = 0;
+		monster.scale.x = 4;
+		monster.scale.y = 4;
 	}
 
-	function setupBook(){ // center the sprite's anchor point
+	function setupBook(){
 		book.anchor.x = 0.5;
 		book.anchor.y = 0.5;
-
-		// move the sprite to the center of the screen
 		book.position.x = 300;
 		book.position.y = 350;
 	}
 
-	function isFuzzyCollision(r1, r2) {
+	function setupRoom(){
+		room.anchor.x = 0;
+		room.anchor.y = 0;
+		room.position.x = 0;
+		room.position.y = 0;
+		room.scale.x = 4;
+		room.scale.y = 4;
+	}
+
+	function setupDresser(){
+		dresser.anchor.x = 0.5;
+		dresser.anchor.y = 0.5;
+		dresser.position.x = 470;
+		dresser.position.y = 245;
+		dresser.scale.x = 4;
+		dresser.scale.y = 4;
+	}
+
+	function isCollision(r1, r2) {
 		return !(r2.x > (r1.x + r1.width) || 
 			(r2.x + r2.width) < r1.x || 
 			r2.y > (r1.y + r1.height) ||
